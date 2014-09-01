df55d1f0ce281d756fea90c458ed6113d7e9e4b0
Prior <fgnodtke@cronosx.de>
2014-09-01 16:41:10 +0200

    Fixed confused textures

--- cubes/heal.js
+++ cubes/heal.js
@@ -1,7 +1,7 @@
 function(callback) {
     getCube("friend.js", function(cube) {
         cube.health = 10;
-		cube.setTexture("heal_20.png");
+		cube.setTexture("heal.png");
         cube.damage = function() {
             cube.health--;
             if(cube.health <= 0) cube.kill();
